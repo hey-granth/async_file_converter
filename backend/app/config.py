4d07d89ba214d71a5b145eac110245073f41230d
Granth Agarwal <heygranth@gmail.com>
2025-06-14 22:34:59 +0530

    redis upstash used

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,14 @@
 from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 REDIS_HOST = getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = int(getenv('REDIS_PORT', 6379))
 REDIS_DB = int(getenv('REDIS_DB', 0))
+REDIS_PASSWORD = getenv('REDIS_PASSWORD')
 
 TEMP_DIR = '/tmp/file_converter'
 
-REDIS_BROKER_URL = getenv('REDIS_BROKER_URL', 'redis://redis:6379/0')
-REDIS_RESULT_BACKEND = getenv('REDIS_RESULT_BACKEND', 'redis://redis:6379/1')
+REDIS_BROKER_URL = getenv('REDIS_BROKER_URL')
+REDIS_RESULT_BACKEND = getenv('REDIS_RESULT_BACKEND')
--- backend/app/utils/redis_ops.py
+++ backend/app/utils/redis_ops.py
@@ -1,10 +1,16 @@
 # to store and retrieve converted file paths from redis
 
 import redis
-from backend.app.config import REDIS_HOST, REDIS_PORT, REDIS_DB
+from backend.app.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
 
 
-redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, decode_responses=True)
+redis_client = redis.StrictRedis(
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    password=REDIS_PASSWORD,
+    decode_responses=True,
+    ssl=True
+)
 
 def store_result_path(task_id: str, file_path: str, ttl_seconds: int = 600):
     """
